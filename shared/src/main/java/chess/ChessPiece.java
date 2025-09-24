b9a3cff3cb2ec724b709ab206c17346aa83b4f70
Benjamin Keller <Benjaminkeller@gmail.com>
2025-09-24 16:37:35 -0600

    added new tests

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -11,11 +11,13 @@ import java.util.List;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public class ChessPiece {
+public class ChessPiece
+{
     private final ChessGame.TeamColor pieceColor;
     private PieceType type;
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type)
+    {
         this.pieceColor = pieceColor;
         this.type = type;
     }
@@ -23,7 +25,8 @@ public class ChessPiece {
     /**
      * The various different chess piece options
      */
-    public enum PieceType {
+    public enum PieceType
+    {
         KING,
         QUEEN,
         BISHOP,
@@ -35,14 +38,16 @@ public class ChessPiece {
     /**
      * @return Which team this chess piece belongs to
      */
-    public ChessGame.TeamColor getTeamColor() {
+    public ChessGame.TeamColor getTeamColor()
+    {
         return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
-    public PieceType getPieceType() {
+    public PieceType getPieceType()
+    {
         return type;
     }
 
@@ -53,9 +58,11 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
-    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition)
+    {
         List<ChessMove> possibleMoves = new ArrayList<>();
-        switch (type) {
+        switch (type)
+        {
             case KING:
                 int[][] kingDirections = new int[][]{{1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {1, 0}, {0, 1}, {-1, 0}, {0, -1}};
                 possibleMoves.addAll(getMoves(board, myPosition, kingDirections, false));
@@ -85,26 +92,33 @@ public class ChessPiece {
         return possibleMoves;
     }
 
-    List<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition, int[][] directions, boolean repeat) {
+    List<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition, int[][] directions, boolean repeat)
+    {
         HashSet<ChessPosition> possiblePositions = new HashSet<>();
-        for (var pos : directions) {
+        for (var pos : directions)
+        {
 
             //Column / x left and right
             //Row / y / up and down
             ChessPosition checkingPos = new ChessPosition(myPosition.getRow() + pos[1], myPosition.getColumn() + pos[0]);
             //check for off board and reperting
-            if (board.isPositionOffBoard(checkingPos)) {
+            if (board.isPositionOffBoard(checkingPos))
+            {
                 continue;
             }
-            if (board.getPiece(checkingPos) == null || board.getPiece(checkingPos).pieceColor != pieceColor) {
+            if (board.getPiece(checkingPos) == null || board.getPiece(checkingPos).pieceColor != pieceColor)
+            {
                 possiblePositions.add(checkingPos);
-                for (int i = 1; !board.isPositionOffBoard(checkingPos) && repeat; i++) {
+                for (int i = 1; !board.isPositionOffBoard(checkingPos) && repeat; i++)
+                {
                     possiblePositions.add(checkingPos);
                     checkingPos = new ChessPosition(myPosition.getRow() + (pos[1] * i), myPosition.getColumn() + (pos[0]) * i);
                     ChessPiece checkingPiece = board.getPiece(checkingPos);
-                    if (checkingPiece == null) {
+                    if (checkingPiece == null)
+                    {
                         continue;
-                    } else if (checkingPiece.pieceColor != pieceColor)// if enemy
+                    }
+                    else if (checkingPiece.pieceColor != pieceColor)// if enemy
                     {
                         possiblePositions.add(checkingPos);
                     }
@@ -117,40 +131,51 @@ public class ChessPiece {
 
         //convert ChessPosition to ChessMoves
         List<ChessMove> possibleMoves = new ArrayList<>();
-        for (var pos : possiblePositions) {
+        for (var pos : possiblePositions)
+        {
             possibleMoves.add(new ChessMove(myPosition, pos, null));
         }
         return new ArrayList<>(possibleMoves);
     }
 
-    List<ChessMove> getMovesPawn(ChessBoard board, ChessPosition myPosition) {
+    List<ChessMove> getMovesPawn(ChessBoard board, ChessPosition myPosition)
+    {
         List<ChessMove> possibleMoves = new ArrayList<>();
         int dir = 0;
-        if (pieceColor == ChessGame.TeamColor.WHITE) {
+        if (pieceColor == ChessGame.TeamColor.WHITE)
+        {
             dir = 1;
-        } else {
+        }
+        else
+        {
             dir = -1;
         }
 
         //move forward
         ChessPosition checkingPos = new ChessPosition(myPosition.getRow() + dir, myPosition.getColumn());
         //this may be invalid position,
-        if (board.isPositionOffBoard(checkingPos)) {
+        if (board.isPositionOffBoard(checkingPos))
+        {
             System.out.printf("pawn move from %s to %s may be invalid", myPosition, checkingPos);
             return possibleMoves;
         }
         // set promotion piece if close
         boolean canPromote = false;
         if (checkingPos.getRow() == 8 && pieceColor == ChessGame.TeamColor.WHITE
-                || checkingPos.getRow() == 1 && pieceColor == ChessGame.TeamColor.BLACK) {
+                || checkingPos.getRow() == 1 && pieceColor == ChessGame.TeamColor.BLACK)
+        {
             canPromote = true;
         }
 
         //check for validity of potential moves
-        if (board.getPiece(checkingPos) == null) {
-            if (!canPromote) {
+        if (board.getPiece(checkingPos) == null)
+        {
+            if (!canPromote)
+            {
                 possibleMoves.add(new ChessMove(myPosition, checkingPos, null));
-            } else {
+            }
+            else
+            {
                 possibleMoves.add(new ChessMove(myPosition, checkingPos, PieceType.QUEEN));
                 possibleMoves.add(new ChessMove(myPosition, checkingPos, PieceType.ROOK));
                 possibleMoves.add(new ChessMove(myPosition, checkingPos, PieceType.BISHOP));
@@ -159,9 +184,11 @@ public class ChessPiece {
 
             //if pawn has not moved yet, check the next square too
             if ((myPosition.getRow() == 2 && pieceColor == ChessGame.TeamColor.WHITE)
-                    || (myPosition.getRow() == 7 && pieceColor == ChessGame.TeamColor.BLACK)) {
+                    || (myPosition.getRow() == 7 && pieceColor == ChessGame.TeamColor.BLACK))
+            {
                 checkingPos = new ChessPosition(myPosition.getRow() + dir * 2, myPosition.getColumn());
-                if (board.getPiece(checkingPos) == null) {
+                if (board.getPiece(checkingPos) == null)
+                {
                     possibleMoves.add(new ChessMove(myPosition, checkingPos, null));
                 }
 
@@ -171,16 +198,22 @@ public class ChessPiece {
         //capture
         canPromote = false; //re-initialize
         if (checkingPos.getRow() == 8 && pieceColor == ChessGame.TeamColor.WHITE
-                || checkingPos.getRow() == 1 && pieceColor == ChessGame.TeamColor.BLACK) {
+                || checkingPos.getRow() == 1 && pieceColor == ChessGame.TeamColor.BLACK)
+        {
             canPromote = true;
         }
 
         ChessPosition checkingCaptureA = new ChessPosition(myPosition.getRow() + dir, myPosition.getColumn() - 1);
-        if (!board.isPositionOffBoard(checkingCaptureA)) {
-            if (board.getPiece(checkingCaptureA) != null && board.getPiece(checkingCaptureA).pieceColor != pieceColor) {
-                if (!canPromote) {
+        if (!board.isPositionOffBoard(checkingCaptureA))
+        {
+            if (board.getPiece(checkingCaptureA) != null && board.getPiece(checkingCaptureA).pieceColor != pieceColor)
+            {
+                if (!canPromote)
+                {
                     possibleMoves.add(new ChessMove(myPosition, checkingCaptureA, null));
-                } else {
+                }
+                else
+                {
                     possibleMoves.add(new ChessMove(myPosition, checkingCaptureA, PieceType.QUEEN));
                     possibleMoves.add(new ChessMove(myPosition, checkingCaptureA, PieceType.ROOK));
                     possibleMoves.add(new ChessMove(myPosition, checkingCaptureA, PieceType.BISHOP));
@@ -189,11 +222,16 @@ public class ChessPiece {
             }
         }
         ChessPosition checkingCaptureB = new ChessPosition(myPosition.getRow() + dir, myPosition.getColumn() + 1);
-        if (!board.isPositionOffBoard(checkingCaptureB)) {
-            if (board.getPiece(checkingCaptureB) != null && board.getPiece(checkingCaptureB).pieceColor != pieceColor) {
-                if (!canPromote) {
+        if (!board.isPositionOffBoard(checkingCaptureB))
+        {
+            if (board.getPiece(checkingCaptureB) != null && board.getPiece(checkingCaptureB).pieceColor != pieceColor)
+            {
+                if (!canPromote)
+                {
                     possibleMoves.add(new ChessMove(myPosition, checkingCaptureB, null));
-                } else {
+                }
+                else
+                {
                     possibleMoves.add(new ChessMove(myPosition, checkingCaptureB, PieceType.QUEEN));
                     possibleMoves.add(new ChessMove(myPosition, checkingCaptureB, PieceType.ROOK));
                     possibleMoves.add(new ChessMove(myPosition, checkingCaptureB, PieceType.BISHOP));
@@ -206,30 +244,36 @@ public class ChessPiece {
 
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return super.toString();
     }
 
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(Object obj)
+    {
         //if memory address is the same
-        if (this == obj) {
+        if (this == obj)
+        {
             return true;
         }
         //if it's a different class
-        if (obj == null || getClass() != obj.getClass()) {
+        if (obj == null || getClass() != obj.getClass())
+        {
             return false;
         }
         //check for reals
         ChessPiece that = (ChessPiece) obj;
-        if (that.type == this.type && that.pieceColor == this.pieceColor) {
+        if (that.type == this.type && that.pieceColor == this.pieceColor)
+        {
             return true;
         }
         return false;
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         return (pieceColor.ordinal() * 7) + (type.ordinal() * 13);
     }
 }
