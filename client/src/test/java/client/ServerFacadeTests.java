78a382a250c97819e65c37f6718b0f647d110e26
Benjamin Keller <Benjaminkeller@gmail.com>
2025-11-06 14:30:13 -0700

    Most of the facade tests
    made them so much simpler this time via
    
    helper functions
    one assert at a time (other tests help cover other pre-checkes)

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,5 +1,8 @@
 package client;
 
+import dataaccess.DataAccessException;
+import datamodel.LoginRequest;
+import datamodel.LogoutRequest;
 import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
@@ -8,15 +11,13 @@ import serverFacade.ServerFacade;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class ServerFacadeTests
-{
+public class ServerFacadeTests {
 
     private static Server server;
     static ServerFacade facade;
 
     @BeforeAll
-    public static void init()
-    {
+    public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
@@ -24,28 +25,72 @@ public class ServerFacadeTests
     }
 
     @AfterAll
-    static void stopServer()
-    {
+    static void stopServer() {
         server.stop();
     }
 
     @BeforeEach
-    void clearDB()
-    {
-
+    void dropDatabase() throws Exception {
+        facade.dropDatabase();
     }
 
     @Test
-    void register() throws Exception
-    {
-        AuthData authData = facade.register("player1", "password", "p1@email.com");
+    void dropDatabaseTest() throws Exception {
+        AuthData authData = registerTestUser();
+        assertDoesNotThrow(() -> facade.login(new LoginRequest(testUser.username(), testUser.password())));
+        facade.dropDatabase();
+        assertThrows(DataAccessException.class, () -> facade.login(new LoginRequest(testUser.username(), testUser.password())));
+    }
+
+    @Test
+    void dropDatabaseTestNegative() throws Exception {
+        facade.dropDatabase();
+        assertThrows(DataAccessException.class, () -> facade.login(new LoginRequest(testUser.username(), testUser.password())));
+    }
+
+    @Test
+    void register() throws Exception {
+        AuthData authData = registerTestUser();
         assertTrue(authData.authToken().length() > 10);
     }
 
     @Test
-    public void sampleTest()
-    {
-        assertTrue(true);
+    void registerTwice() throws Exception {
+        registerTestUser();
+        assertThrows(DataAccessException.class, () -> registerTestUser());
+    }
+
+    @Test
+    void loginGood() throws Exception {
+        registerTestUser();
+        AuthData authData = facade.login(new LoginRequest(testUser.username(), testUser.password()));
+        assertTrue(authData.authToken().length() > 10);
+    }
+
+    @Test
+    void loginBad() throws Exception {
+        registerTestUser();
+        assertThrows(DataAccessException.class, () -> facade.login(new LoginRequest(testUser2.username(), testUser2.password())));
+    }
+
+    @Test
+    void logoutGood() throws Exception {
+        AuthData authData = registerTestUser();
+        assertDoesNotThrow(() -> facade.logout(new LogoutRequest(authData.authToken())));
+    }
+
+    @Test
+    void logoutBad() throws Exception {
+        registerTestUser();
+        assertThrows(DataAccessException.class, () -> facade.logout(new LogoutRequest("bad auth token sajkdfhsjhdf")));
+    }
+
+    //helper functions and vars
+    private static UserData testUser = new UserData("player1", "password", "p1@email.com");
+    private static UserData testUser2 = new UserData("player2", "password2", "p2@email.com");
+
+    AuthData registerTestUser() throws Exception {
+        return facade.register(testUser.username(), testUser.password(), testUser.email());
     }
 
 }
