58474abec856e76137d6fe66a5f206b521835cae
Benjamin Keller <Benjaminkeller@gmail.com>
2025-11-13 00:26:30 -0700

    print board out test 1

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import chess.ChessBoard;
+import chess.ChessMove;
+import repl.GameRepl;
 import repl.PreLoginRepl;
 
 public class Main {
@@ -6,6 +9,9 @@ public class Main {
         String serverURL = "http://localhost:8080";
         if (args.length == 1)
             serverURL = args[0];
+        ChessBoard cb = new ChessBoard();
+        cb.resetBoard();
+        System.out.print(GameRepl.buildBoard(cb));
         new PreLoginRepl(serverURL).run();
     }
 }
--- client/src/main/java/repl/GameRepl.java
+++ client/src/main/java/repl/GameRepl.java
@@ -2,6 +2,7 @@ package repl;
 
 import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessPiece;
 import chess.ChessPosition;
 import dataaccess.DataAccessException;
 import serverFacade.ServerFacade;
@@ -57,14 +58,52 @@ public class GameRepl extends Repl {
                 """ + getAwaitUserInputText();
     }
 
-    private String buildBoard(ChessBoard chessBoard) {
+    public static String buildBoard(ChessBoard chessBoard) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= 8; i++) {
-            for (int j = 1; j <= 8; j++) {
-                sb.append(chessBoard.getPiece(new ChessPosition(i, j)));
+        sb.append(SET_TEXT_BOLD);
+        boolean white = false;
+        for (int i = 9; i >= 0; i--) {
+            for (int j = 0; j <= 9; j++) {
+                if (i == 0 || i == 9 || j == 0 || j == 9) { //boarder
+                    sb.append(SET_BG_COLOR_LIGHT_GREY);
+                    if ((j == 0 || j == 9) && i != 0 && i != 9)
+                        sb.append(" " + i + " ");
+                    else if ((i == 0 || i == 9) && j != 0 && j != 9)
+                        sb.append(" " + (char) ('a' + j - 1) + " ");
+                    else
+                        sb.append(EMPTY);
+                } else {
+                    if (white) {
+                        sb.append(SET_BG_COLOR_WHITE);
+                    } else {
+                        sb.append(SET_BG_COLOR_BLACK);
+                    }
+                    white = !white;
+                    ChessPiece chessPiece = chessBoard.getPiece(new ChessPosition(i, j));
+                    if (chessPiece != null) {
+                        if (chessPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                            sb.append(SET_TEXT_COLOR_RED);
+                        } else {
+                            sb.append(SET_TEXT_COLOR_BLUE);
+                        }
+                        switch (chessPiece.getPieceType()) {
+                            case PAWN -> sb.append(" P ");
+                            case ROOK -> sb.append(" R ");
+                            case QUEEN -> sb.append(" Q ");
+                            case KNIGHT -> sb.append(" N ");
+                            case BISHOP -> sb.append(" B ");
+                            case KING -> sb.append(" K ");
+                        }
+                    } else {
+                        sb.append(EMPTY);
+                    }
+                    sb.append(RESET_TEXT_COLOR);
+                }
             }
-            sb.append('\n');
+            sb.append(RESET_BG_COLOR + RESET_TEXT_COLOR + '\n');
+            white = !white;
         }
+        sb.append(RESET_TEXT_BOLD_FAINT);
         return sb.toString();
     }
 }
