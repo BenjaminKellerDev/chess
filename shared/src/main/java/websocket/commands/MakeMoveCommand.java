db609378eb0131fba3126144a58fd76a71db21ba
Benjamin Keller <Benjaminkeller@gmail.com>
2025-12-01 23:04:13 -0700

    Create MakeMoveCommand.java

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,37 @@
+package websocket.commands;
+
+import chess.ChessMove;
+
+import java.util.Objects;
+
+public class MakeMoveCommand extends UserGameCommand {
+    private final ChessMove move;
+
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
+        super(commandType, authToken, gameID);
+        this.move = move;
+    }
+
+    public ChessMove getMove() {
+        return move;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof MakeMoveCommand that)) {
+            return false;
+        }
+        return getCommandType() == that.getCommandType() &&
+                Objects.equals(getAuthToken(), that.getAuthToken()) &&
+                Objects.equals(getMove(), that.getMove()) &&
+                Objects.equals(getGameID(), that.getGameID());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCommandType(), getAuthToken(), getGameID(), getMove());
+    }
+}
