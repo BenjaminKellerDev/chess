71ba60d595194e3cef33a40c12f6b4e9823d8fa2
Benjamin Keller <Benjaminkeller@gmail.com>
2025-12-02 18:53:39 -0700

    made connection manager

--- server/src/main/java/server/ConnectionManager.java
+++ server/src/main/java/server/ConnectionManager.java
@@ -3,21 +3,41 @@ package server;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
+import java.io.IOException;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
+    public final ConcurrentHashMap<Integer, Set<Session>> games = new ConcurrentHashMap<>();
+
+    public void add(int gameID, Session session) {
+        if (!games.containsKey(gameID)) {
+            games.put(gameID, ConcurrentHashMap.newKeySet());
+        }
+        games.get(gameID).add(session);
+    }
+
+    public void remove(int gameID, Session session) {
+        if (games.containsKey(gameID)) {
+            games.get(gameID).remove(session);
+        }
+        if (games.get(gameID).size() == 0) {
+            games.remove(gameID);
+        }
+    }
+
+    public void broadcast(int gameID, Session excludeSession, ServerMessage message) throws IOException {
+        for (Session s : games.get(gameID)) {
+            if (s.isOpen()) {
+                if (!s.equals(excludeSession)) {
+                    s.getRemote().sendString(message.toString());
+                }
+            }
+        }
+    }
+}
+
+class connections {
     public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
 
-    public void add(Session session) {
-        connections.put(session, session);
-    }
-
-    public void remove(Session session) {
-        connections.remove(session);
-    }
-
-    public void broadcast(Session excludeSession, ServerMessage message) {
-        String msg = message.toString();
-        
-    }
 }
